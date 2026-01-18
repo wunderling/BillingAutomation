6a575703a9e24ddc2f1715f01487d7db27616235
FinVisTech <finvision24@gmail.com>
2026-01-18 10:32:42 -0800

    web host update

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
     const { searchParams, origin } = new URL(request.url)
     const code = searchParams.get('code')
     const next = searchParams.get('next') ?? '/dashboard'
